b28284eda2f9f5aeefaa7eef9ddd9eba2ab6a1b4
Kat Imperial <imperialkatherine@gmail.com>
2022-04-15 16:37:22 -0400

    set up useEffect slash onChange for Log In page

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const LogIn = () => {
+
+    const [email, setEmail] = useState('')
+    useEffect(() => {console.log(email)}, [email])
+
+    const [password, setPassword] = useState('')
+    useEffect(() => {console.log(password)}, [password])
 
     return (
         <div>
             <form>
                 <label>
                     Email:
-                    <input type="text" />
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
                 </label>
                 <label>
                     Password:
-                    <input type="text" />
+                    <input type="text" value={password} onChange={e => setPassword(e.target.value)}/>
                 </label>
             </form>
         </div>
