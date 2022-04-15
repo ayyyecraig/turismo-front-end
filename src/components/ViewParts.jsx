549a757f6f3f37cdba9203ea30ccea7112071f95
Kat Imperial <imperialkatherine@gmail.com>
2022-04-15 15:24:21 -0400

    added sample info to parts page

--- src/components/ViewParts.jsx
+++ src/components/ViewParts.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 const ViewParts = () => {
 
+    return (
+        <div>
+            <h1>Buy New Parts</h1>
+            <h2>Muffler:</h2>
+            <img src="https://cartreatments.com/wp-content/uploads/rust-hole-exhaust.jpg"/>
+            <ul>
+                <li>Type:</li>
+                <li>Weight:</li>
+                <li>Effect on Speed:</li>
+                <li>Effect on Acceleration:</li>
+                <li>Effect on Horsepower:</li>
+                <li>Price:</li>
+            </ul>
+        </div>
+    )
+
+
 }
 
 export default ViewParts
