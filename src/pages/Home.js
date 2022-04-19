d44a5d32c9c70320098de881a51ca5b590e33e59
Adelaide Craig <adelaide.craig@outlook.com>
2022-04-19 08:50:01 -0500

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,40 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
 
 
-const Home = () => {
-  let navigate = useNavigate()
 
-  return (
-    <div className="home-container col">
+const Home = ({authenicated, user}) => {
+
+  let authenicatedOptions
+  if(user){
+    authenicatedOptions = (
+      
+        <div className="home-container col">
+          <h1>Welcome to Car App</h1>
+
+          <button onClick={() => navigate('/garage')   }>
+           Click Here To Get Started
+          </button>
+         
+        </div>
+      )
+  }
+  const publicOptions = (
+        <div className="home-container col">
             <h1>Welcome to Car App</h1>
 
       <section className="welcome-signin">
-        <button onClick={() => navigate('/signin')}>
+        <button onClick={() => navigate('/login')}>
           Click Here To Get Started
         </button>
       </section>
     </div>
   )
-}
 
+  return (
+    <div>
+    {authenticated && user ? authenticatedOptions : publicOptions}
+    </div>
+  )
+
+}
 export default Home
