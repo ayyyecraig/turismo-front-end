be8970571a0a0fa2688e63c72b806a4517cbc84a
Adelaide Craig <adelaide.craig@outlook.com>
2022-04-19 08:30:02 -0500

    auth nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,12 +7,15 @@ const Nav = ({ authenticated, user, handleLogOut }) => {
     if(user){
         authenticatedOptions = (
             <nav>
-                <h3>Hello, {user.email}</h3>
+                <Link to="/">Home</Link>
                 <Link to="/garage">Garage</Link>
                 {/* 11-13 might need to be modified once we test our run thru on the app */}
                 <Link onClick={handleLogOut} to="/">
                     Log Out
                 </Link> 
+                <Link to="/cars">Car Lot</Link>
+                <Link to="/parts">Specs and Mods</Link>
+                <h3>Hello, {user.email}</h3>
             </nav>
         )
     }
